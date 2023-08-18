fcc923df11a9e69aa2e4c55baf32c3c9183d3486
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2023-08-18 20:15:44 +0300

    add task 7

---
+++ task-7_5.js
@@ -0,0 +1,28 @@
+// # Завдання 5
+// Створіть функцію personInfo, яка приймає об'єкт 
+// в якості параметра та виконує деструктуризацію цього об'єкта, 
+// витягуючи з нього наступні поля: "name", "age", "email", "city". 
+// Потім використайте деструктуризовані значення для створення нового 
+// об'єкта з наступними полями: 
+// "fullName" (значення поля "name" додане до значення поля "age" через пробіл), 
+// "contact" (значення поля "email" додане до значення поля "city" через кому). 
+// Функція має повертати цей новий об'єкт.
+// Маємо наступний об'єкт:
+
+const person = {
+  name: "John Doe",
+  age: 30,
+  email: "john.doe@example.com",
+  city: "New York",
+};
+
+function personInfo({name, age, email, city}){
+    return {
+        contact: `${email}, ${city}`,
+        fullName: `${name}, ${age}`,
+    }
+}
+console.log(personInfo(person))
+// Маєте отримати результат:
+// contact: "john.doe@example.com, New York"
+// fullName: "John Doe, 30"
